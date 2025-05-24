dbc582f1fcf9c13bd89a5fc83b60292e5808b9e1
mdanikislam <anikislam487@gmail.com>
2025-05-24 14:35:36 +0600

    load data from local storage

--- src/Components/Bottles/Bottles.jsx
+++ src/Components/Bottles/Bottles.jsx
@@ -1,34 +1,57 @@
 import { useEffect, useState } from "react";
 import Bottle from "../Bottle/Bottle";
-import './Bottles.css'
+import "./Bottles.css";
+import { addToLS, getStoredCart } from "../Utilities/localstorage";
 const Bottles = () => {
-    const [bottles, setBottles] = useState([])
-    const [cart, setCart] = useState([]);
+  const [bottles, setBottles] = useState([]);
+  const [cart, setCart] = useState([]);
 
-    useEffect(()=>{
-        fetch('bottles.json')
-        .then(res=>res.json())
-        .then(data=>setBottles(data))
-    },[])
-    
-    // handle cart 
-    const handleBottleCart = bottle=>{
-        const newCart = [...cart, bottle]
-        setCart(newCart);
-        
+  useEffect(() => {
+    fetch("bottles.json")
+      .then((res) => res.json())
+      .then((data) => setBottles(data));
+  }, []);
+
+//   load cart from localstorage 
+useEffect(()=>{
+    if(bottles.length){
+        const storedCart = getStoredCart();
+
+            const saveCart = [];
+        for(const id of storedCart){
+            const bottle = bottles.find(bottle => bottle.id === id)
+            if(bottle){
+                saveCart.push(bottle);
+            }
+            
+        }
+        setCart(saveCart)
     }
+},[bottles])
 
-    return (
-        <div>
-            <h2>Chose Your Best Bottle</h2>
-            <h3>Cart : {cart.length}</h3>
-          <div className="bottles-container">
-             {
-            bottles.map(bottle=><Bottle handleBottleCart={handleBottleCart} key={bottle.id} bottle={bottle}></Bottle>)
-           } 
-          </div>
-        </div>
-    );
+
+  // handle cart
+  const handleBottleCart = (bottle) => {
+    const newCart = [...cart, bottle];
+    setCart(newCart);
+    addToLS(bottle.id);
+  };
+
+  return (
+    <div>
+      <h2>Chose Your Best Bottle</h2>
+      <h3>Cart : {cart.length}</h3>
+      <div className="bottles-container">
+        {bottles.map((bottle) => (
+          <Bottle
+            handleBottleCart={handleBottleCart}
+            key={bottle.id}
+            bottle={bottle}
+          ></Bottle>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Bottles;
---
+++ src/Components/Utilities/localstorage.js
@@ -0,0 +1,22 @@
+const getStoredCart = ()=>{
+    const storedCartString = localStorage.getItem('cart');
+    if(storedCartString){
+        return JSON.parse(storedCartString)
+    }
+    return []
+}
+
+const saveCartToLS = cart =>{
+    const cartStrigified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStrigified);
+}
+
+const addToLS = id =>{
+    const cart = getStoredCart();
+    cart.push(id);
+
+    // save to localstorage
+    saveCartToLS(cart); 
+}
+
+export {addToLS, getStoredCart}
